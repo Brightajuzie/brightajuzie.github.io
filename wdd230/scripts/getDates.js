2ecf39414b41ef21c3b0616d8001254407dd32d8
unknown <brightajuzie@gmail.com>
2025-04-01 01:14:07 +0100

    pdate

--- wdd230/scripts/getDates.js
+++ wdd230/scripts/getDates.js
@@ -20,4 +20,20 @@ modeButton.addEventListener("click", () => {
 		main.style.color = "#000";
 		modeButton.textContent = "🕶️";
 	}
-});
+});                                                                                                                                                                                                                                                                                                                     querySelector(".visits");
+
+ 
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+ 
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+ 
+numVisits++;
+
+ 
+localStorage.setItem("numVisits-ls", numVisits);
+ 
